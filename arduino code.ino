5dba75912b0b1c0e8a7f35d30865990476119a54
Varsha Shiburaj <varshashiburaj@gmail.com>
2019-12-22 12:55:54 +0530

    Create arduino code.ino

---
+++ arduino code.ino
@@ -0,0 +1,70 @@
+char c='s';
+
+
+void setup()  
+{
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  Serial2.println("bLuEtOoTh InItIaTeD....");
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  
+  digitalWrite(8,HIGH);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,HIGH);
+  Serial1.println("Stop");
+}
+
+void loop()
+{
+   
+    if(Serial2.available())    ////Bluetooth control
+    {
+       c=Serial2.read();
+    }
+   delay(80);
+     if(c=='s')
+   {
+     digitalWrite(8,HIGH);
+     digitalWrite(9,HIGH);
+     digitalWrite(10,HIGH);
+     digitalWrite(11,HIGH);
+     Serial2.println("Stop");
+   }
+   else if(c=='a')
+   {
+     digitalWrite(8,LOW);
+     digitalWrite(9,HIGH);
+     digitalWrite(10,LOW);
+     digitalWrite(11,HIGH);
+     Serial2.println("Forward");
+   }
+   else if(c=='b')
+   {
+     digitalWrite(8,HIGH);
+     digitalWrite(9,LOW);
+     digitalWrite(10,HIGH);
+     digitalWrite(11,LOW);
+     Serial2.println("Reverse");
+   }
+   else if(c=='c')
+   {
+     digitalWrite(8,LOW);
+     digitalWrite(9,HIGH);
+     digitalWrite(10,HIGH);
+     digitalWrite(11,LOW);
+     Serial2.println("Right");
+   }
+   else if(c=='d')
+   {
+     digitalWrite(8,HIGH);
+     digitalWrite(9,LOW);
+     digitalWrite(10,LOW);
+     digitalWrite(11,HIGH);
+     Serial2.println("Left");
+   }
+}
+
